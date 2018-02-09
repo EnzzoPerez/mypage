491048bbbe40f41635f73b27bb86dde60dba1d7d
Enzzo <enzzorp@gmail.com>
2018-02-09 02:00:15 -0300

    fix styles and updated links

--- src/page.js
+++ src/page.js
@@ -18,7 +18,7 @@ export default class Page extends React.Component {
         </div>
         <div className="socialMedia">
             <a href="https://github.com/EnzzoPerez" target="_blank" rel="nofollow noopener noreferrer"><i id="github" className="fab fa-github-square fa-5x" ></i></a>
-            <a href="https://twitter.com/enzoperez_" target="_blank" rel="nofollow noopener noreferrer"><i id="twitter" className="fab fa-twitter-square fa-5x"></i></a>
+            <a href="https://twitter.com/enzzoperez_" target="_blank" rel="nofollow noopener noreferrer"><i id="twitter" className="fab fa-twitter-square fa-5x"></i></a>
             <a onClick={this.showAlert}><i className="fas fa-envelope-square fa-5x"></i></a>
         </div>
       </div>
