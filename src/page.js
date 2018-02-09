6923dfd9ea8a357b9578aca07a7cbf82a9363275
Enzzo <enzzorp@gmail.com>
2018-02-08 23:06:40 -0300

    update config

--- src/page.js
+++ src/page.js
@@ -11,18 +11,15 @@ export default class Page extends React.Component {
     return (      
       <div className="contain">
         <div className="name">
-            Enzo Perez
-            <div className="balon">
-              <img src="./assets/images/balon.png" className="App-logo" alt="pelota" />
-            </div>
+        $<span className="guionBajo">_ </span> Enzo Perez
         </div>
         <div className="dev">
-          Frontend Dev & Frustrated Footballer 😋
+          Frontend Dev  (& Frustrated Footballer 😋)
         </div>
         <div className="socialMedia">
-            <a href="https://github.com/EnzzoPerez" target="_blank" rel="nofollow noopener noreferrer"><img src="./assets/images/github.svg" className="" alt="" /></a>
-            <a href="https://twitter.com/knockEnzzo" target="_blank" rel="nofollow noopener noreferrer"><img src="./assets/images/twitter.svg" className="" alt="" /></a>
-            <a onClick={this.showAlert}><img src="./assets/images/email.svg" className="" alt="" /></a>
+            <a href="https://github.com/EnzzoPerez" target="_blank" rel="nofollow noopener noreferrer"><i id="github" className="fab fa-github-square fa-5x" ></i></a>
+            <a href="https://twitter.com/enzoperez_" target="_blank" rel="nofollow noopener noreferrer"><i id="twitter" className="fab fa-twitter-square fa-5x"></i></a>
+            <a onClick={this.showAlert}><i className="fas fa-envelope-square fa-5x"></i></a>
         </div>
       </div>
 
