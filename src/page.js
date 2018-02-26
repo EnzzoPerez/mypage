b7237db4e655185ddebc4dfac8b5a579982b5bed
Enzzo <enzzorp@gmail.com>
2018-02-26 11:21:01 -0300

    fix divs that no leave click social media links

--- src/page.js
+++ src/page.js
@@ -16,15 +16,16 @@ export default class Page extends React.Component {
         <div className="dev">
           Frontend Dev  (& Frustrated Footballer 😋)
         </div>
+        <div className="link">
+          <a href="https://enzzoperez.github.io/my-cv" target="_blank" rel="nofollow noopener noreferrer">Link to my web resume</a>
+        </div>
         <div className="socialMedia">
             <a href="https://github.com/EnzzoPerez" target="_blank" rel="nofollow noopener noreferrer"><i id="github" className="fab fa-github-square fa-5x" ></i></a>
             <a href="https://twitter.com/enzzoperez_" target="_blank" rel="nofollow noopener noreferrer"><i id="twitter" className="fab fa-twitter-square fa-5x"></i></a>
             <a href="https://www.linkedin.com/in/enzoperezm/" target="_blank" rel="nofollow noopener noreferrer"><i id="twitter" className="fab fa-linkedin fa-5x"></i></a>
             <a onClick={this.showAlert}><i className="fas fa-envelope-square fa-5x"></i></a>
         </div>
-        <div className="link">
-          <a href="https://enzzoperez.github.io/my-cv" target="_blank" rel="nofollow noopener noreferrer">Link to my web resume</a>
-        </div>
+        
       </div>
 
     );
